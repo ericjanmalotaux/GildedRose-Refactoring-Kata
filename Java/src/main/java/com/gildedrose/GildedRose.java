78f039594a896dacafa8351e4cdec4e0a92577b2
malota <Ericjan.Malotaux@kadaster.nl>
2019-06-19 19:08:57 +0200

    Extract decrease quality by 1 into a method.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,9 +13,7 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
                 if (!item.name.equals("Aged Brie")
                         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    degrade(item);
                 } else {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
@@ -40,9 +38,7 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     if (!item.name.equals("Aged Brie")) {
                         if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
+                            degrade(item);
                         } else {
                             item.quality = 0;
                         }
@@ -53,6 +49,12 @@ class GildedRose {
                     }
                 }
             }
+        }
+    }
+
+    private void degrade(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 }
