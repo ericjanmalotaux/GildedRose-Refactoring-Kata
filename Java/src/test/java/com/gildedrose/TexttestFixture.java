46ef98d9aa997ea9f75784b2b815f81eb994f710
malota <Ericjan.Malotaux@kadaster.nl>
2019-06-19 16:09:19 +0200

    Use the vararg constructor to cleanup the TexttestFixture a bit.

--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -4,7 +4,7 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
+        GildedRose app = new GildedRose(
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -14,9 +14,7 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
-
-        GildedRose app = new GildedRose(items);
+                new Item("Conjured Mana Cake", 3, 6));
 
         int days = 2;
         if (args.length > 0) {
@@ -26,7 +24,7 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
+            for (Item item : app.items) {
                 System.out.println(item);
             }
             System.out.println();
