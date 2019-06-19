8d8caaabf6a36f985e7d51a69bb1c012f9bbbc14
malota <Ericjan.Malotaux@kadaster.nl>
2019-06-19 19:02:00 +0200

    Daardoor kunnen alle andere condities op "Sulfuras..." nu weg.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,9 +14,7 @@ class GildedRose {
                 if (!item.name.equals("Aged Brie")
                         && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     if (item.quality < 50) {
@@ -43,9 +41,7 @@ class GildedRose {
                     if (!item.name.equals("Aged Brie")) {
                         if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                             if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
+                                item.quality = item.quality - 1;
                             }
                         } else {
                             item.quality = item.quality - item.quality;
