cf68d5af7ab47392a3b33a2d974c07c081f9603d
malota <Ericjan.Malotaux@kadaster.nl>
2019-06-20 10:13:46 +0200

    Inline and remove improve.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,21 +14,21 @@ class GildedRose {
             }
             item.sellIn = item.sellIn - 1;
             if (item.name.equals("Aged Brie")) {
-                improve(item, 1);
+                degrade(item, -1);
 
 
                 if (item.sellIn < 0) {
-                    improve(item, 1);
+                    degrade(item, -1);
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                improve(item, 1);
+                degrade(item, -1);
 
                 if (item.sellIn < 10) {
-                    improve(item, 1);
+                    degrade(item, -1);
                 }
 
                 if (item.sellIn < 5) {
-                    improve(item, 1);
+                    degrade(item, -1);
                 }
 
 
@@ -44,10 +44,6 @@ class GildedRose {
                 }
             }
         }
-    }
-
-    private void improve(Item item, int amount) {
-        degrade(item, -amount);
     }
 
     private void degrade(Item item, int amount) {
