7458c026fc8fc1c8075fdf3757ad24edfa5dd486
Eric Jan Malotaux <emalotau@gmail.com>
2022-04-27 21:34:31 +0200

    Decrement of sellIn at the beginning of the loop.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,18 +6,19 @@ class GildedRose(val items: Array<Item>) {
         items
             .filterNot { item -> item.name == "Sulfuras, Hand of Ragnaros" }
             .forEach { item ->
+                item.sellIn = item.sellIn - 1
                 if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
 
                         if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                            if (item.sellIn < 11) {
+                            if (item.sellIn < 10) {
                                 if (item.quality < 50) {
                                     item.quality = item.quality + 1
                                 }
                             }
 
-                            if (item.sellIn < 6) {
+                            if (item.sellIn < 5) {
                                 if (item.quality < 50) {
                                     item.quality = item.quality + 1
                                 }
@@ -29,9 +30,6 @@ class GildedRose(val items: Array<Item>) {
                         item.quality = item.quality - 1
                     }
                 }
-
-                item.sellIn = item.sellIn - 1
-
                 if (item.sellIn < 0) {
                     if (item.name == "Aged Brie") {
                         if (item.quality < 50) {
