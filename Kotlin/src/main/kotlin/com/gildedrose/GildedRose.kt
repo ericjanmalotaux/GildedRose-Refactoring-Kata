27e3ade0abc404afdcc51ad2de8e1ca66a6149ed
Eric Jan Malotaux <emalotau@gmail.com>
2022-04-27 21:31:06 +0200

    Inverted conditions to make them positive.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,11 +6,7 @@ class GildedRose(val items: Array<Item>) {
         items
             .filterNot { item -> item.name == "Sulfuras, Hand of Ragnaros" }
             .forEach { item ->
-                if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1
-                    }
-                } else {
+                if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
 
@@ -28,22 +24,26 @@ class GildedRose(val items: Array<Item>) {
                             }
                         }
                     }
+                } else {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1
+                    }
                 }
 
                 item.sellIn = item.sellIn - 1
 
                 if (item.sellIn < 0) {
-                    if (item.name != "Aged Brie") {
-                        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name == "Aged Brie") {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    } else {
+                        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                            item.quality = item.quality - item.quality
+                        } else {
                             if (item.quality > 0) {
                                 item.quality = item.quality - 1
                             }
-                        } else {
-                            item.quality = item.quality - item.quality
-                        }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
                         }
                     }
                 }
