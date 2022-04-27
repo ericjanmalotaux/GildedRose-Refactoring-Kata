a19cb1f230d60b775fcba26ec267ea1d87e88c27
Eric Jan Malotaux <emalotau@gmail.com>
2022-04-27 21:57:57 +0200

    Moved extra increment for Brie when sellIn becomes negative.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -11,6 +11,11 @@ class GildedRose(val items: Array<Item>) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
                     }
+                    if (item.sellIn < 0) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
                 } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
@@ -33,11 +38,7 @@ class GildedRose(val items: Array<Item>) {
                     }
                 }
                 if (item.sellIn < 0) {
-                    if (item.name == "Aged Brie") {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    } else {
+                    if (item.name != "Aged Brie") {
                         if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                             item.quality = 0
                         } else {
