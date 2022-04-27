7295df1f505542eb496de1ff279d2672d63fc692
Eric Jan Malotaux <emalotau@gmail.com>
2022-04-27 22:18:18 +0200

    Use increment and decrement operators.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -11,11 +11,11 @@ class GildedRose(val items: Array<Item>) {
                     sellIn = sellIn - 1
                     if (name == "Aged Brie") {
                         if (quality < 50) {
-                            quality = quality + 1
+                            quality += 1
                         }
                         if (sellIn < 0) {
                             if (quality < 50) {
-                                quality = quality + 1
+                                quality += 1
                             }
                         }
                     } else if (name == "Backstage passes to a TAFKAL80ETC concert") {
@@ -23,26 +23,26 @@ class GildedRose(val items: Array<Item>) {
                             quality = 0
                         } else {
                             if (quality < 50) {
-                                quality = quality + 1
+                                quality += 1
                                 if (sellIn < 10) {
                                     if (quality < 50) {
-                                        quality = quality + 1
+                                        quality += 1
                                     }
                                 }
                                 if (sellIn < 5) {
                                     if (quality < 50) {
-                                        quality = quality + 1
+                                        quality += 1
                                     }
                                 }
                             }
                         }
                     } else {
                         if (quality > 0) {
-                            quality = quality - 1
+                            quality -= 1
                         }
                         if (sellIn < 0) {
                             if (quality > 0) {
-                                quality = quality - 1
+                                quality -= 1
                             }
                         }
                     }
