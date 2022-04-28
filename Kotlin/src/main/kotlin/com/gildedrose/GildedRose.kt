9af07972e48086c93da0c08ad816984a4ec52d19
Eric Jan Malotaux <emalotau@gmail.com>
2022-04-28 23:15:28 +0200

    private as much as possible.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,8 +1,11 @@
 package com.gildedrose
 
+private fun Int.depreciate(step: Int = 1) = (this - step).takeIf { it >= 0 } ?: this
+private fun Int.improve(step: Int = 1) = (this + step).takeIf { it <= 50 } ?: this
+
 class GildedRose(var items: Array<Item>) {
 
-    val strategies = mapOf(
+    private val strategies = mapOf(
         "Aged Brie" to Brie::updateQuality,
         "Backstage passes to a TAFKAL80ETC concert" to BackstagePass::updateQuality,
         "Sulfuras, Hand of Ragnaros" to Sulfuras::updateQuality
@@ -12,17 +15,17 @@ class GildedRose(var items: Array<Item>) {
         items = items.map { (strategies[it.name] ?: Normal::updateQuality).invoke(it) }.toTypedArray()
     }
 
-    object Normal {
+    private object Normal {
         fun updateQuality(item: Item) =
             (item.sellIn - 1).let { Item(item.name, it, item.quality.depreciate(if (it >= 0) 1 else 2)) }
     }
 
-    object Brie {
+    private object Brie {
         fun updateQuality(item: Item) =
             (item.sellIn - 1).let { Item(item.name, it, item.quality.improve(if (it >= 0) 1 else 2)) }
     }
 
-    object BackstagePass {
+    private object BackstagePass {
         fun updateQuality(item: Item) =
             (item.sellIn - 1).let {
                 Item(
@@ -40,11 +43,7 @@ class GildedRose(var items: Array<Item>) {
             }
     }
 
-    object Sulfuras {
+    private object Sulfuras {
         fun updateQuality(item: Item) = item
     }
-
 }
-
-fun Int.depreciate(step: Int = 1) = (this - step).takeIf { it >= 0 } ?: this
-fun Int.improve(step: Int = 1) = (this + step).takeIf { it <= 50 } ?: this
